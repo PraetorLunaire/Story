0737204969a5ff294704b8869d304db667d13b07
Rubens <rubens.paulico@hotmail.com>
2019-04-24 16:35:37 -0300

    Initial Commit

---
+++ Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by Rubens Paulico on 24/04/19.
+//  Copyright © 2019 Rubens Paulico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
