e341804e711ca06d62e8700ba57f096c4e40a099
Rubens <rubens.paulico@hotmail.com>
2019-04-27 23:41:45 -0300

    Criado pasta Attributes para ícones de atributos, pasta Mental, Social e Physical Adicionado 12 ícones para as opções

--- Story/startScreen.swift
+++ Story/startScreen.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     
     var starting = false
+    var seeming = [0, 0, 0, 0, 0, 0]
+    //Seeming Beast = seeming[0]
+    //Seeming Darkling = seeming[1]
+    //Seeming Elemental = seeming[2]
+    //Seeming Fairest = seeming[3]
+    //Seeming Ogre = seeming[4]
+    //Seeming Wizened = seeming[5]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +32,15 @@ class ViewController: UIViewController {
         storyStart.alpha = 0.01
         //startButton.alpha = 0
         UIView.animate(withDuration: 5, delay: 0, options: [.repeat, .autoreverse], animations: {
-            //
-            //            if self.starting == false {
-            //                // animaçao do botao
-            //                UIButton.animate(withDuration: 3, delay: 0, options: [], animations: {
-            //                    self.startButton.alpha = 1
-            //
-            //                }, completion: nil)
-            //                self.starting = true
-            //            }
+//            
+//                        if self.starting == false {
+//                            // animaçao do botao
+//                            UIButton.animate(withDuration: 3, delay: 0, options: [], animations: {
+//                                self.startButton.alpha = 1
+//            
+//                            }, completion: nil)
+//                            self.starting = true
+//                        }
             self.storyStart.alpha = 1
         }, completion: nil)
         
